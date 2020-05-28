c693e3ab8ee918283cc78e8be57bb4792839d34b
Adrian <adrberia@gmail.com>
2020-05-27 22:07:51 -0400

    Updates

---
+++ precache-manifest.4328314074b6b2ad8b3cff99b2544131.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765a5fdfe12c5e9d1ab224d434762c7f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "969863eca9290f0c6e90",
+    "url": "/portfolio/static/css/main.c2d3d048.chunk.css"
+  },
+  {
+    "revision": "4b8090542342f966da7a",
+    "url": "/portfolio/static/js/2.d7740abe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.d7740abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969863eca9290f0c6e90",
+    "url": "/portfolio/static/js/main.b2d100ad.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "d1e4b3b5c708ae098623e518627b841d",
+    "url": "/portfolio/static/media/bitcoin.d1e4b3b5.png"
+  },
+  {
+    "revision": "81bbc2eee0c0bd8cb2c519112a857e94",
+    "url": "/portfolio/static/media/blog.81bbc2ee.png"
+  },
+  {
+    "revision": "267f44e7cd8223b5f2f950daf1ed2f83",
+    "url": "/portfolio/static/media/covid-project.267f44e7.png"
+  },
+  {
+    "revision": "672fcab994ad8222def501a5f40f3632",
+    "url": "/portfolio/static/media/dev-connector.672fcab9.png"
+  },
+  {
+    "revision": "ea5edbeaf071b3f1cf5bfd600eb770f8",
+    "url": "/portfolio/static/media/domoviz.ea5edbea.png"
+  },
+  {
+    "revision": "131c60ce342c05e9bbf361b53f183b92",
+    "url": "/portfolio/static/media/profile-image.131c60ce.jpg"
+  },
+  {
+    "revision": "410f01e7360c1e44c83bf3985afdff7b",
+    "url": "/portfolio/static/media/property-brothers.410f01e7.png"
+  }
+]);
